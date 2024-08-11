fdde71f26b4c7fd62fdfa6e43cf313aab5efce82
rahul <rahul@cdspace.in>
2024-08-11 22:23:29 +0530

    add ON and OFF words bitmap

--- Core/Inc/ssd1306.h
+++ Core/Inc/ssd1306.h
@@ -186,6 +186,5 @@ void ssd1306_WriteCommand(uint8_t byte);
 void ssd1306_WriteData(uint8_t* buffer, size_t buff_size);
 SSD1306_Error_t ssd1306_FillBuffer(uint8_t* buf, uint32_t len);
 
-_END_STD_C
 
 #endif // __SSD1306_H__
